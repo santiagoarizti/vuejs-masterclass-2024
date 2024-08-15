c5c0618076c8cab34e4b5616f1304a9c858b67ac
Santiago Arizti <santiago@cyberpuerta.mx>
2024-08-15 17:54:59 -0600

    starting to consume projects data

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 
-const projects = ref<any>()
+const projects = ref<any[] | null>(null)
 
 ;(async () => {
   const { data, error } = await supabase.from('projects').select()
@@ -17,5 +17,10 @@ const projects = ref<any>()
   <div>
     <h1>Home Page</h1>
     <RouterLink to="/projects">Go to projects</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project }}
+      </li>
+    </ul>
   </div>
 </template>
