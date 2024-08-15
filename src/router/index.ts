b29e022ac2648a31d00e9cf4ef87f9b1c63be354
Santiago Arizti <santiago@cyberpuerta.mx>
2024-08-15 14:28:38 -0600

    create main views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
+import HomeView from '@/views/HomeView.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue')
+    }
+  ]
 })
 
 export default router
