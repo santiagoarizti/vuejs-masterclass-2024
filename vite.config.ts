218b024166b286441d94e3e55a2a5ae8af651a47
Santiago Arizti <santiago@cyberpuerta.mx>
2024-08-15 14:47:24 -0600

    refactor for unplugin vue router

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import vueRouter from 'unplugin-vue-router/vite'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vueRouter(), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
