8b96e5fa5e6ee5bcb9db75e39b0130acb368e573
Santiago Arizti <santiago@cyberpuerta.mx>
2024-08-16 13:58:29 -0600

    adding shadcn-vue components avatar, dropdown, input

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+</script>
 
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
     <form class="w-fill max-w-96">
-      <input class="w-full pl-8 bg-background" type="text" placeholder="Search whatever" />
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
     </form>
-    <div class="w-8 h-8 rounded-full bg-white"></div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
