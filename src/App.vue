97e9aefeba5e43b796140347d4aade3f9c79e526
Santiago Arizti <santiago@cyberpuerta.mx>
2024-08-19 12:28:34 -0600

    extract sidebar components

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,14 @@
 <script setup lang="ts">
-import { Input } from '@/components/ui/input'
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import TopNavbar from '@/components/layout/TopNavbar.vue'
+import Sidebar from '@/components/layout/Sidebar.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
-    <form class="relative w-fill h-fit max-w-96">
-      <iconify-icon
-        class="absolute left-2.5 top-[50%] translate-y-[-50%] tnra text-muted-foreground"
-        icon="lucide:search"
-      ></iconify-icon>
-      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
