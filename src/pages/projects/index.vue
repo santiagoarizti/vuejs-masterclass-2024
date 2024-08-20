69b46774af3444e7aa5f6485d112e57afb621839
Santiago Arizti <santiago@cyberpuerta.mx>
2024-08-19 18:40:25 -0600

    extract queries

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
+import { projectsQuery } from '@/utils/supaQueries'
+import type { Projects } from '@/utils/supaQueries'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Tables<'projects'>[] | null>(null)
+const projects = ref<Projects | null>(null)
 
 const getProjects = async () => {
-  const { data, error } = await supabase.from('projects').select()
+  const { data, error } = await projectsQuery
 
   if (error) console.log(error)
 
@@ -18,7 +18,7 @@ const getProjects = async () => {
 
 await getProjects()
 
-const columns: ColumnDef<Tables<'projects'>>[] = [
+const columns: ColumnDef<Projects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
